e34291033e9f7623603d970090131e51c9a017bd
Juan Sebastian <jsmo012005@gmail.com>
2025-03-02 17:29:39 -0500

    Adding specific amounts of indentation for web dev

--- lua/kagatto/optionsLanguages.lua
+++ lua/kagatto/optionsLanguages.lua
@@ -4,6 +4,10 @@ vim.cmd([[
 
         " HTML
         autocmd FileType html setlocal expandtab shiftwidth=2 tabstop=2 softtabstop=2
+        autocmd FileType css setlocal expandtab shiftwidth=2 tabstop=2 softtabstop=2
+        autocmd FileType javascript setlocal expandtab shiftwidth=2 tabstop=2 softtabstop=2
+        autocmd FileType typescript setlocal expandtab shiftwidth=2 tabstop=2 softtabstop=2
+        autocmd FileType typescriptreact setlocal expandtab shiftwidth=2 tabstop=2 softtabstop=2
 
     augroup END
 ]])
